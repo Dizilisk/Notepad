8d0184485a437127b025b511dbdeec86d2d1a33b
Dizilisk <andrepidenko@yandex.ru>
2021-07-07 16:25:24 +0300

    new'

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+object Main {
+
+        class User {
+
+            val user1 = new User {
+                val name = "Andrey"
+                val number = 1
+            }
+            val x = user1.name
+            val y = user1.number
+            println(x, y)
+        }
+    def main(args: Array[String]): Unit = {
+        println("a")
+    }
+}
+
