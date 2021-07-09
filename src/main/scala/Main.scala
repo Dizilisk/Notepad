eb87e63d695e26faa82e33186be114d27cffc889
Dizilisk <andrepidenko@yandex.ru>
2021-07-09 23:14:54 +0300

    toString override

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,13 @@
 object Main {
 
     case class User(name: String, number: Int) {
-
+        override def toString: String = {
+            this.name + this.number
+        }
     }
 
     def main(args: Array[String]): Unit = {
-        val user1 = User("Andrey", 1)
+        val user1 = User("Andrey, ", 1)
         println(user1)
     }
 }
