bbaf290a01b8c9a5475666078fc3bb7962d544a0
Dizilisk <andrepidenko@yandex.ru>
2021-07-19 19:52:10 +0300

    add Map

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,16 @@
 object Main {
-
     case class User(name: String, number: Int) {
         override def toString: String = {
             this.name + this.number
         }
     }
+
+//    def getKey(Map): Option[]
     def main(args: Array[String]): Unit = {
         val user1 = User("Andrey, ", 1)
-        println(user1)
+        val user2 = User("Andrey2, ", 2)
+        val user3 = User("Andrey3, ", 3)
+        val Collection = Map(1 -> user1, 2 -> user2, 3 -> user3)
+        println(Collection(1))
     }
 }
-
